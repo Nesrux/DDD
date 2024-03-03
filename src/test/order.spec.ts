531086a080b1d3a51d094475014dc5a74160443b
Nesrux <joaomarcosdevs@gmail.com>
2024-03-03 14:05:22 -0300

    test: refatorando os testes de order

--- src/test/order.spec.ts
+++ src/test/order.spec.ts
@@ -4,14 +4,14 @@ import OrderItem from "../entity/Order_Item";
 describe("order unit test", () => {
   it("shold throw error when id is empty", () => {
     expect(() => {
-      let order = new Order("", "1", [new OrderItem("a1", "product1", 10)]);
+      let order = new Order("", "1", [new OrderItem("a1", "product1", 10, "p1", 1)]);
     }).toThrowError("Id is required");
 
   })
 
   it("shold throw error when CustumerId is empty", () => {
     expect(() => {
-      let order = new Order("1", "", [new OrderItem("a1", "product1", 10)]);
+      let order = new Order("1", "", [new OrderItem("a1", "product1", 10, "p1", 1)]);
     }).toThrowError("CustumerId is required");
   })
 
@@ -19,13 +19,13 @@ describe("order unit test", () => {
   it("shold throw error when lenght is 0", () => {
     expect(() => {
       let order = new Order("1", "1", []);
-    }).toThrowError("itens are required ");
+    }).toThrowError("itens are required");
   })
   it("shold calculate total", () => {
-    const item = new OrderItem("1", "item1", 180)
-    const item2 = new OrderItem("2", "item2", 80)
-    const item3 = new OrderItem("3", "item3", 50)
-    const totalPrice = item.price + item2._price + item3.price;
+    const item = new OrderItem("1", "item1", 180, "p1", 2)
+    const item2 = new OrderItem("2", "item2", 80, "p2", 1)
+    const item3 = new OrderItem("3", "item3", 50, "p3", 4)
+    const totalPrice = item.price + item2.price + item3.price;
 
     const order = new Order("a1", "b1", [item, item2, item3])
     const total = order.total();
