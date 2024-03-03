6658604d0d9d7893582e90558345df79708c98ce
Nesrux <joaomarcosdevs@gmail.com>
2024-03-03 14:55:31 -0300

    chore: utilizando o tsc ao invés do jest

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import OrderItem from "./entity/Order_Item";
 
 // Customer aggregate
 let custumer = new Customer("123", "jaumm")
-const addres = new Address("rua dois", 2, "123456789", "sao paulo", "são sebastiao");
-custumer.address = addres;
+const addres = new Address("rua dois", 2, "123456789", "sao paulo");
+custumer.Address = addres;
 custumer.activate;
 
 // Order aggregate
-const item1 = new OrderItem("1", "item 1", 10);
-const item2 = new OrderItem("2", "item 2", 15);
+const item1 = new OrderItem("1", "item 1", 10, "p1", 1);
+const item2 = new OrderItem("2", "item 2", 15, "p2", 2);
 const order = new Order("1", "123", [item1, item2]);
