5b8049b789c48610b3ce750d7bfbbba0431b59fa
Nesrux <joaomarcosdevs@gmail.com>
2024-03-04 18:34:42 -0300

    feat: adiciona gets a classe de Address

--- src/domain/entity/Address.ts
+++ src/domain/entity/Address.ts
@@ -3,7 +3,7 @@ export default class Address {
   _number: number = 0;
   _zip: string = "";
   _city: string = "";
-  
+
   constructor(street: string, number: number, zip: string, city: string) {
     this._street = street;
     this._number = number;
@@ -12,6 +12,24 @@ export default class Address {
 
     this.validate();
   }
+
+
+  public get street(): string {
+    return this._street;
+  }
+
+  public get number(): number {
+    return this._number;
+  }
+
+  public get zip(): string {
+    return this._zip;
+  }
+
+  public get city(): string {
+    return this._city;
+  }
+
   validate() {
     if (this._street.length === 0) {
       throw new Error("Street is required");
