0c57164d6f0c3899ed54249064d683597312e2a1
Nesrux <joaomarcosdevs@gmail.com>
2024-03-05 15:50:43 -0300

    test: cria test para unregister e unregister all events

--- src/domain/event/@shared/event-dispacher.spec.ts
+++ src/domain/event/@shared/event-dispacher.spec.ts
@@ -10,5 +10,37 @@ describe("domain events test", () => {
 
     expect(eventDispacher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
     expect(eventDispacher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+    expect(eventDispacher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler)
   });
+
+  it("shold unregister an event handler", () => {
+    const eventDispacher = new EventDispacher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispacher.register("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispacher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
+
+    eventDispacher.unregister("ProductCreatedEvent", eventHandler)
+
+    expect(eventDispacher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+
+    expect(eventDispacher.getEventHandlers["ProductCreatedEvent"].length).toBe(0)
+
+  })
+
+  it("shold unregister all events", () => {
+    const eventDispacher = new EventDispacher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispacher.register("ProductCreatedEvent", eventHandler);
+    
+    expect(eventDispacher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
+
+
+    eventDispacher.unregisterAll();
+
+    expect(eventDispacher.getEventHandlers).toStrictEqual({})
+
+  })
 });
