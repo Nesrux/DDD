9bff23f895799ed01e71bde0472f874c44f89e18
Nesrux <joaomarcosdevs@gmail.com>
2024-03-05 12:49:56 -0300

    test: adiciona teste para criação de orders

--- src/infrastructure/repository/Order.repository.spec.ts
+++ src/infrastructure/repository/Order.repository.spec.ts
@@ -30,37 +30,49 @@ describe("Order repository test", () => {
     await sequelize.close();
   });
 
-  it("shold create a new Order", async () => {
+  it("should create a new order", async () => {
     const customerRepository = new CustomerRepository();
-    const customer = new Customer("123", "Customer 1")
-    const address = new Address("rua 1", 1, "codigo 1", "cidade 1")
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
     customer.changeAddress(address);
-    await customerRepository.create(customer)
+    await customerRepository.create(customer);
 
     const productRepository = new ProductRepository();
-    const product = new Product("123", "product 1", 100);
+    const product = new Product("123", "Product 1", 10);
     await productRepository.create(product);
 
-    const orderItem = new OrderItem("1", product.name, product.price, product.id, 2)
+    const ordemItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
 
-    const order = new Order("123", customer.id, [orderItem])
+    const order = new Order("123", "123", [ordemItem]);
 
     const orderRepository = new OrderRepository();
-    await orderRepository.create(order)
+    await orderRepository.create(order);
 
-    const orderModel = await OrderModel.findOne({ where: { id: order.id }, include: ["items"] });
+    const orderModel = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ["items"],
+    });
 
     expect(orderModel.toJSON()).toStrictEqual({
       id: "123",
-      customer_id: customer.id,
-      items: [{
-        id: orderItem.id,
-        name: orderItem.name,
-        price: orderItem.price,
-        quantity: orderItem.quantity,
-        product_id: product.id
-      }],
+      customer_id: "123",
       total: order.total(),
+      items: [
+        {
+          id: ordemItem.id,
+          name: ordemItem.name,
+          price: ordemItem.price,
+          quantity: ordemItem.quantity,
+          order_id: "123",
+          product_id: "123",
+        },
+      ],
     });
   });
 
--- src/infrastructure/repository/Order.repository.ts
+++ src/infrastructure/repository/Order.repository.ts
@@ -5,18 +5,23 @@ import OrderItemModel from "../db/sequelize/model/OrderItem.model";
 export default class OrderRepository {
 
   async create(entity: Order): Promise<void> {
-    await OrderModel.create({
-      id: entity.id,
-      customer_id: entity.customerId,
-      items: entity.items.map((item) => ({
-        id: item.id,
-        price: item.price,
-        name: item.name,
-        product_id: item.productId,
-        quantity: item.quantity
-      })),
-    }, { include: [{ model: OrderItemModel }] })
+    await OrderModel.create(
+      {
+        id: entity.id,
+        customer_id: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        include: [{ model: OrderItemModel }],
+      }
+    );
   }
-
 
 }
