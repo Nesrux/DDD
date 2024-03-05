e01107dc310d0a860c35cd15d94d3ab3b694a06e
Nesrux <joaomarcosdevs@gmail.com>
2024-03-05 15:31:50 -0300

    feat: cria event de registro

--- src/domain/event/@shared/event-dispacher.spec.ts
+++ src/domain/event/@shared/event-dispacher.spec.ts
@@ -1,7 +1,10 @@
+import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-Email-When-Product-Is-created-Handler";
+import EventDispacher from "./event-dispacher";
+
 describe("domain events test", () => {
   it("should register an event handler", () => {
     const eventDispacher = new EventDispacher();
-    const eventHandler = new SendEmailWhenProductIsHandler();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
 
     eventDispacher.register("ProductCreatedEvent", eventHandler);
 
